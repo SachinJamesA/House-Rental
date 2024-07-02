af263b83055df1871b5d210664ac6d22d39a5a70
SachinJamesA <143333095+SachinJamesA@users.noreply.github.com>
2024-07-02 21:57:37 +0530

    Second commit

--- src/myComponents/About.js
+++ src/myComponents/About.js
@@ -7,8 +7,21 @@ export default function About() {
           <img className="w-[30rem]" src="../assets/logo.png" alt="Background"/>
         </div>
         <div className='my-8 w-[100%] flex justify-center'>
-          <div className='flex justify-center  w-[70%]'>
-            <p className=' flex justify-center items-center text-base '>Launched in 2005, 99acres.com, India’s No. 1 property portal, deals with every aspect of the consumers’ needs in the real estate industry. It is an online forum where buyers, sellers and brokers/agents can exchange information about real estate properties quickly, effectively and inexpensively. At 99acres.com, you can advertise a property, search for a property, browse through properties, build your own property microsite, and keep yourself updated with the latest news and trends making headlines in the realty sector.</p>
+          <div className='w-[70%]'>
+            <p className=' flex justify-center items-center text-base '>Launched in 2005, 99acres.com, India's No. 1 property portal, deals with every aspect of the consumers needs in the real estate industry. It is an online forum where buyers, sellers and brokers/agents can exchange information about real estate properties quickly, effectively and inexpensively. At 99acres.com, you can advertise a property, search for a property, browse through properties, build your own property microsite, and keep yourself updated with the latest news and trends making headlines in the realty sector.</p>
+            <h2>Why Choose Us?</h2>
+            <p>At present, 99acres.com prides itself for having around nine lakh property listings spanning across 600+ cities in India. Of all, the website held over 5.7 lakh paid listings at the end of FY 2018-19. In addition to providing an online platform to real estate developers, brokers and property owners for listing their property for sale, purchase or rent, 99acres.com offers advertisement stints such as microsites, banners, home page links and project pages to the clients for better visibility and branding in the market.
+            With the ever-evolving online search behaviour, 99acres.com shares updated information pertinent to real estate activities, assisting prospective buyers to make informed buying decision. We make online property search easier, quicker and smarter!</p>
+            <h2>Awards And Recognition</h2>
+            <p>• PropTech Mobile App of the Year Award 2019</p>
+            <p>• PropTech Mobile App of the Year Award 2019</p>
+            <p>• PropTech Mobile App of the Year Award 2019</p>
+            <p>• PropTech Mobile App of the Year Award 2019</p>
+            <p>• PropTech Mobile App of the Year Award 2019</p>
+            <p>• PropTech Mobile App of the Year Award 2019</p>
+            <div>
+              <h4>Connect With Us</h4>
+            </div>
           </div>
         </div>
     </div>
